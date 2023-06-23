713cc720569b67e5d326abb807544ad1ad0c03c7
Egor Martynov <egmaegma02@gmail.com>
2023-06-23 23:04:00 +0500

    save changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,26 @@ function App() {
             <div>
                 <div className='avatar'>
                     <span className='avatar_text'>СТЕЛЛА FACEBOOK</span>
-                    <img src="src/photo_2023-06-23_22-13-25.jpg" className='avatar_img' alt=""/>
+                    <img src="src/assets/photo_2023-06-23_22-13-25.jpg" className='avatar_img' alt=""/>
                 </div>
                 <div className='main'>
                     <span className='main_text'>ЩОБ ПОЧАТИ <span className='green_text'>ЗАРОБЛЯТИ</span>, ВИБЕРИ ЗРУЧНИЙ  ДЛЯ ТЕБЕ МЕСЕНДЖЕР</span>
                 </div>
                 <div className='buttons'>
                     <div className='button button-vb'>
-                        <img className='viber logo' src="src/viber.png" alt=""/>
+                        <img className='viber logo' src="src/assets/viber.png" alt=""/>
                         <a href="viber://chat?number=%2B380666995051" className='vb_text'>
                             Написати у Viber
                         </a>
                     </div>
                     <div className='button button-tg'>
-                        <img className='tg logo' src="src/telegram.png" alt=""/>
+                        <img className='tg logo' src="src/assets/telegram.png" alt=""/>
                         <a href="" className='tg_text'>
                             Написати у Telegram
                         </a>
                     </div>
                     <div className='button button-inst'>
-                        <img className='inst logo' src="src/instagram.png" alt=""/>
+                        <img className='inst logo' src="src/assets/instagram.png" alt=""/>
                         <a href="" className='inst_text'>
                             Написати у Instagram
                         </a>
